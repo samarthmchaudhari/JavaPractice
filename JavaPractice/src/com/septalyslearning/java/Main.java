d8bc4c3520491736c5cff49ffb41bd13ef8db361
d <d@Samarth.mshome.net>
2018-03-17 20:44:28 +0530

    Initial Checkin

---
+++ JavaPractice/src/com/septalyslearning/java/Main.java
@@ -0,0 +1,15 @@
+package com.septalyslearning.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Mobile m1=new Mobile();
+		m1.setMobileID("1");
+		m1.setMobileType("Android");
+		m1.setMobileCompany("MI");
+
+		
+	}
+
+}
---
+++ JavaPractice/src/com/septalyslearning/java/Mobile.java
@@ -0,0 +1,29 @@
+package com.septalyslearning.java;
+
+public class Mobile {
+
+	private String mobileID;
+	private String mobileType;
+	private String mobileCompany;
+	
+	public String getMobileID() {
+		return mobileID;
+	}
+	public void setMobileID(String mobileID) {
+		this.mobileID = mobileID;
+	}
+	public String getMobileType() {
+		return mobileType;
+	}
+	public void setMobileType(String mobileType) {
+		this.mobileType = mobileType;
+	}
+	public String getMobileCompany() {
+		return mobileCompany;
+	}
+	public void setMobileCompany(String mobileCompany) {
+		this.mobileCompany = mobileCompany;
+	}
+
+	
+}
